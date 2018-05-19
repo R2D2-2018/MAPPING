fb761cd018d9bd915cbdf82f9a1d61c43c20ac72
Login-is-already-taken <nemkerunkszorolapot@outlook.com>
2018-05-19 17:59:37 +0200

    Added map2d class

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "../src/vector2d.hpp"
+#include "../src/map2d.hpp"
+#include "../src/angle.hpp"
 
 TEST_CASE("Vector2d", "[Vector2d]")
 {
@@ -12,4 +14,19 @@ TEST_CASE("Vector2d", "[Vector2d]")
     REQUIRE(vec3.x == 5);
     REQUIRE(vec3.y == 6);
     REQUIRE(vec1.length() == 5);
+}
+
+
+TEST_CASE("Map2d", "[Map2d]")
+{
+    Mapping::Map2d<10, 10> map(Mapping::Vector2d(5, 5), 3);
+
+    //Set sensor pos to [3, 2];
+    map.moveSensorCm(Mapping::Vector2d(-6, -9));
+    REQUIRE(map.getSensorPosition().x == 3);
+    REQUIRE(map.getSensorPosition().y == 2);
+
+    //Set [4, 3] as impassable
+    map.setRelativePointAsImpassable(Mapping::Vector2d(3, 3));
+    REQUIRE(map.getGrid()[4][3] == true);
 }
