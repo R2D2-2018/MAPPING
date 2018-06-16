204b94c220d8412944c83757935362974ce86ca9
Nick <nickswaerdens@gmail.com>
2018-06-16 21:19:46 +0200

    Converted to updated Arduino build environment, fixed tests.

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,7 @@ int main() {
     hwlib::wait_ms(1000);
     hwlib::cout << "Hello world!" << hwlib::endl;
 
+    // hwlib::target::pin_out a = hwlib::target::pin_out(hwlib::target::pins::d10);
+    
     return 0;
 }
